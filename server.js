8faa430df723a4532bfcabe17390c1096a3292e2
Tristan Hudreaux <contact@brico-technique.fr>
2026-09-10 16:34:33 +0200

    Ajout interface web

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("public"));
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
